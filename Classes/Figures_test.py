640d18eb27f211f430fd4300c796d6cabb44da48
staticbg <staticbg@gmail.com>
2014-04-20 15:41:49 +0300

    added unit test for the figure classes

---
+++ Classes/Figures_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from Figures import Pawn, Knight, Bishop, Rook, Queen, King
+
+
+class TestFigures(unittest.TestCase):
+    def test_to_string(self):
+        self.assertEqual(str(Pawn('White')), 'White Pawn')
+        self.assertEqual(str(Knight('White')), 'White Knight')
+        self.assertEqual(str(Bishop('White')), 'White Bishop')
+        self.assertEqual(str(Rook('Black')), 'Black Rook')
+        self.assertEqual(str(Queen('Black')), 'Black Queen')
+        self.assertEqual(str(King('Black')), 'Black King')
+
+    def test_equals(self):
+        self.assertEqual(Pawn('White'), Pawn('White'))
+        self.assertNotEqual(Pawn('White'), Pawn('Black'))
+        self.assertNotEqual(Pawn('White'), Knight('White'))
+
+if __name__ == '__main__':
+    unittest.main()
