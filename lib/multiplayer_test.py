7219749ab14d4b9130d0df18127f53cad92743af
staticbg <staticbg@gmail.com>
2014-05-30 01:09:37 +0300

    added a couple of real games in multiplayer tests and found bug in is_in_checkmate method

--- lib/multiplayer_test.py
+++ lib/multiplayer_test.py
@@ -51,6 +51,90 @@ class TestValidMoves(unittest.TestCase):
         self.assertEqual(game._player_white.show_lost_figures(),
                          ' White Pawn White Knight')
 
+    def test_with_real_games(self):
+        # http://www.chessgames.com/perl/chessgame?gid=1018910
+        game = MultiPlayer('Adolf Anderssen',
+                           'Lionel Adalbert Bagration Felix Kieseritsky')
+
+        game.move('e2', 'e4')
+        game.move('e7', 'e5')
+        game.move('f2', 'f4')
+        game.move('e5', 'f4')
+        game.move('f1', 'c4')
+        game.move('d8', 'h4')
+        game.move('e1', 'f1')
+        game.move('b7', 'b5')
+        game.move('c4', 'b5')
+        game.move('g8', 'f6')
+        game.move('g1', 'f3')
+        game.move('h4', 'h6')
+        game.move('d2', 'd3')
+        game.move('f6', 'h5')
+        game.move('f3', 'h4')
+        game.move('h6', 'g5')
+        game.move('h4', 'f5')
+        game.move('c7', 'c6')
+        game.move('g2', 'g4')
+        game.move('h5', 'f6')
+        game.move('h1', 'g1')
+        game.move('c6', 'b5')
+        game.move('h2', 'h4')
+        game.move('g5', 'g6')
+        game.move('h4', 'h5')
+        game.move('g6', 'g5')
+        game.move('d1', 'f3')
+        game.move('f6', 'g8')
+        game.move('c1', 'f4')
+        game.move('g5', 'f6')
+        game.move('b1', 'c3')
+        game.move('f8', 'c5')
+        game.move('c3', 'd5')
+        game.move('f6', 'b2')
+        game.move('f4', 'd6')
+        game.move('c5', 'g1')
+        game.move('e4', 'e5')
+        game.move('b2', 'a1')
+        game.move('f1', 'e2')
+        game.move('b8', 'a6')
+        game.move('f5', 'g7')
+        game.move('e8', 'd8')
+        game.move('f3', 'f6')
+        game.move('g8', 'f6')
+        game.move('d6', 'e7')
+
+        self.assertEqual(game.move('a3', 'a5'), 'Player Adolf Anderssen wins')
+        self.assertEqual(game._player_black.show_lost_figures(),
+                         ' Black Pawn Black Pawn Black Pawn')
+        print(game._player_black.show_lost_figures())
+        print(game._player_white.show_lost_figures())
+
+        # http://www.chessgames.com/perl/chessgame?gid=1243022
+        game = MultiPlayer('Gioachino Greco', 'NN')
+
+        game.move('e2', 'e4')
+        game.move('b7', 'b6')
+        game.move('d2', 'd4')
+        game.move('c8', 'b7')
+        game.move('f1', 'd3')
+        game.move('f7', 'f5')
+        game.move('e4', 'f5')
+        game.move('b7', 'g2')
+        game.move('d1', 'h5')
+        game.move('g7', 'g6')
+        game.move('f5', 'g6')
+        game.move('g8', 'f6')
+        game.move('g6', 'h7')
+        game.move('f6', 'h5')
+        game.move('d3', 'g6')
+
+        self.assertEqual(game.move('a3', 'a5'),
+                         'Player Gioachino Greco wins')
+        print(game._player_black.show_lost_figures())
+        print(game._player_white.show_lost_figures())
+        self.assertEqual(game._player_white.show_lost_figures(),
+                         ' White Pawn White Queen')
+        self.assertEqual(game._player_black.show_lost_figures(),
+                         ' Black Pawn Black Pawn Black Pawn')
 
 # if __name__ == '__main__':
 #    unittest.main()
